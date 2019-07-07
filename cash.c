203c4d77d7cc4915a3a274b3fbbb9fa627bd7664
carstenstann <carsten.stann@gmail.com>
2019-07-07 16:45:49 +0200

    Update cash.c

--- cash.c
+++ cash.c
@@ -5,6 +5,7 @@
 int main(void)
 {
     float amt;
+    
     // Prompt user for amount
     do
     {
@@ -12,34 +13,37 @@ int main(void)
     }
     while(amt < 0);
     
+    // Calculate total number of cents
     int change = round(amt*100);
     
-    int quarters = 0;
-    int dimes = 0;
-    int nickels = 0;
-    int pennies = 0;
+    int quarters = 0, dimes = 0, nickels = 0, pennies = 0;
     
+    // Calculate number of quarters needed
     while(change >= 25)
     {
         quarters++;
         change -= 25;
     }
+    // Calculate number of dimes needed
     while(change >= 10)
     {
         dimes++;
         change -= 10;
     }
+    // Calculate number of nickels needed
     while(change >= 5)
     {
         nickels++;
         change -=5;
     }
+    // Calculate number of pennies needed
     while(change >= 1)
     {
         pennies++;
         change--;
     }
     
+    // Sum number of coins
     int min_coins = quarters + dimes + nickels + pennies;
       
     printf("%i\n", min_coins);
